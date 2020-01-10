477ce74097e1cd83fa836baee3357a08aa6a69df
OlgaEva <olga.rosas@gmail.com>
2020-01-10 12:56:13 -0400

    create snake file

---
+++ snake.py
@@ -0,0 +1,14 @@
+# FreeCodeCamp's Snake Tutorial Python
+
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object):
+    rows = 0
+    w = 0
+
+class snake(object):
+    pass
